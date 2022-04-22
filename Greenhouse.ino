1c84a75fcd7446072d519f5fb172280e4725897f
zenya <zenya451@protonmail.com>
2022-04-22 20:27:07 +0300

    Добавлена передача данных через UDP

--- Greenhouse.ino
+++ Greenhouse.ino
@@ -1,13 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include "ThingSpeak.h"
+#include <ThingSpeak.h>
 #include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFiUdp.h>
 
-#define MSG_BUFFER_SIZE  (50)
+#define MSG_BUFFER_SIZE  (10)
 #define ONE_WIRE_BUS D3
 
 uint8_t deviceCount = 0;
@@ -25,6 +25,10 @@ const char* Write_API_Key_3 = "";
 
 unsigned long lastMqttMsg = 0;
 unsigned long lastThingMsg = 0;
+unsigned long lastUdpMsg = 0;
+
+bool s1 = true;
+bool s2 = true;
 
 char msg[MSG_BUFFER_SIZE];
 char temp1[MSG_BUFFER_SIZE];
@@ -36,6 +40,7 @@ PubSubClient client(espClient);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress temperatureSensors[3];
+WiFiUDP Udp;
 
 void setup() {
   Serial.begin(115200);
@@ -59,6 +64,7 @@ void loop() {
   client.loop();
   MQTT();
   thingspeak();
+  udp();
   ArduinoOTA.handle();
 }
 
@@ -125,20 +131,58 @@ void MQTT() {
 
 void thingspeak() {
   unsigned long nowT = millis();
-  if (nowT - lastThingMsg == 20000) {
+  if (nowT - lastThingMsg >= 7200000 && s1 == true) {
     sensors.requestTemperatures();
     float t_1 = sensors.getTempC(temperatureSensors[0]);
     int httpCode_1 = ThingSpeak.writeField(Channel_ID_1, 1, t_1, Write_API_Key_1);
+    s1 = false;
   }
-  if (nowT - lastThingMsg == 40000) {
+  if (nowT - lastThingMsg >= 7230000 && s2 == true) {
     sensors.requestTemperatures();
     float t_2 = sensors.getTempC(temperatureSensors[1]);
     int httpCode_2 = ThingSpeak.writeField(Channel_ID_2, 1, t_2, Write_API_Key_2);
+    s2 = false;
   }
-  if (nowT - lastThingMsg == 60000) {
+  if (nowT - lastThingMsg >= 7260000) {
     sensors.requestTemperatures();
     float t_3 = sensors.getTempC(temperatureSensors[2]);
     int httpCode_3 = ThingSpeak.writeField(Channel_ID_3, 1, t_3, Write_API_Key_3);
     lastThingMsg = nowT;
+    s1 = true;
+    s2 = true;
+  }
+}
+
+void udp(){
+  unsigned long nowU = millis();
+  if (nowU - lastUdpMsg > 10000) {
+    lastUdpMsg = nowU;
+    Serial.print("udp");
+    
+    sensors.requestTemperatures();
+    
+    char str1[30] = "Sensor 1: ";
+    char str2[30] = "Sensor 2: ";
+    char str3[30] = "Sensor 3: ";
+    
+    float t1 = sensors.getTempC(temperatureSensors[0]);
+    snprintf (temp1, MSG_BUFFER_SIZE, "%f", t1);
+    float t2 = sensors.getTempC(temperatureSensors[1]);
+    snprintf (temp2, MSG_BUFFER_SIZE, "%f", t2);
+    float t3 = sensors.getTempC(temperatureSensors[2]);
+    snprintf (temp3, MSG_BUFFER_SIZE, "%f", t3);
+    
+    strcat(str1, temp1);
+    strcat(str2, temp2);
+    strcat(str3, temp3);
+    
+    Udp.beginPacket("192.168.0.101", 4210);
+    
+    Udp.write(str1);
+    Udp.endPacket();
+    Udp.write(str2);
+    Udp.endPacket();
+    Udp.write(str3);
+    Udp.endPacket();
   }
 }
