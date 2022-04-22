65a24bccfe619d2600a83b72ae1df0c0ee67a43a
zenya <zenya451@protonmail.com>
2022-04-22 19:55:23 +0300

    Добавлена поддержка датчика температуры ds18b20

--- Greenhouse.ino
+++ Greenhouse.ino
@@ -4,8 +4,13 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define MSG_BUFFER_SIZE  (50)
+#define ONE_WIRE_BUS D3
+
+uint8_t deviceCount = 0;
 
 const char* ssid = "";
 const char* password = "";
@@ -28,7 +33,9 @@ char temp3[MSG_BUFFER_SIZE];
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+DeviceAddress temperatureSensors[3];
 
 void setup() {
   Serial.begin(115200);
@@ -37,6 +44,11 @@ void setup() {
   ThingSpeak.begin(espClient);
   ArduinoOTA.setHostname("ESP8266");
   ArduinoOTA.begin();
+  sensors.begin();
+  deviceCount = sensors.getDeviceCount();
+  for (uint8_t index = 0; index < deviceCount; index++){
+    sensors.getAddress(temperatureSensors[index], index);
+  }
 }
 
 
@@ -91,20 +103,22 @@ void reconnect() {
 
 void MQTT() {
   unsigned long nowM = millis();
-  if (nowM - lastMqttMsg > 2000) {
+  if (nowM - lastMqttMsg > 1000) {
     lastMqttMsg = nowM;
     
+    sensors.requestTemperatures();
+    
     //long t1 = (WiFi.RSSI() * -1);
-    long t1 = random(-20, 40);
-    snprintf (temp1, MSG_BUFFER_SIZE, "%ld", t1);
+    float t1 = sensors.getTempC(temperatureSensors[0]);
+    snprintf (temp1, MSG_BUFFER_SIZE, "%f", t1);
     client.publish("Topic1", temp1);
 
-    long t2 = random(-20, 40);
-    snprintf (temp2, MSG_BUFFER_SIZE, "%ld", t2);
+    float t2 = sensors.getTempC(temperatureSensors[1]);
+    snprintf (temp2, MSG_BUFFER_SIZE, "%f", t2);
     client.publish("Topic2", temp2);
 
-    long t3 = random(-20, 40);
-    snprintf (temp3, MSG_BUFFER_SIZE, "%ld", t3);
+    float t3 = sensors.getTempC(temperatureSensors[2]);
+    snprintf (temp3, MSG_BUFFER_SIZE, "%f", t3);
     client.publish("Topic3", temp3);
   }
 }
@@ -112,15 +126,18 @@ void MQTT() {
 void thingspeak() {
   unsigned long nowT = millis();
   if (nowT - lastThingMsg == 20000) {
-    int t_1 = random(-20, 40);
+    sensors.requestTemperatures();
+    float t_1 = sensors.getTempC(temperatureSensors[0]);
     int httpCode_1 = ThingSpeak.writeField(Channel_ID_1, 1, t_1, Write_API_Key_1);
   }
   if (nowT - lastThingMsg == 40000) {
-    int t_2 = random(-20, 40);
+    sensors.requestTemperatures();
+    float t_2 = sensors.getTempC(temperatureSensors[1]);
     int httpCode_2 = ThingSpeak.writeField(Channel_ID_2, 1, t_2, Write_API_Key_2);
   }
   if (nowT - lastThingMsg == 60000) {
-    int t_3 = random(-20, 40);
+    sensors.requestTemperatures();
+    float t_3 = sensors.getTempC(temperatureSensors[2]);
     int httpCode_3 = ThingSpeak.writeField(Channel_ID_3, 1, t_3, Write_API_Key_3);
     lastThingMsg = nowT;
   }
